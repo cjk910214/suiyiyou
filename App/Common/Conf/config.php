c6f72fc2cf19bc69a172bf21a4dd08e778e4cbf5
jingkanchen <303341452@qq.com>
2017-09-18 13:54:47 +0800

    TP3.2基础框架

---
+++ App/Common/Conf/config.php
@@ -0,0 +1,5 @@
+<?php
+return array(
+    //加载数据库扩展配置
+    'LOAD_EXT_CONFIG' => 'db',
+);
---
+++ App/Common/Conf/db.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cjk
+ * Date: 2017/9/18 0018
+ * Time: 上午 9:54
+ */
+return array(
+    //采用PDO的方式连接mysql
+    'DB_DSN' => 'mysql://root:123456@localhost:3306/suiyiyou#utf8',
+);
